d9e9d3084a34fd5f327f90c5cb95062f49c14a24
chengyouxiao <chengyou.xiao@gmail.com>
2024-05-03 22:58:22 -0400

    Move letters in center, also mapped symbol to name so can use in file name

---
+++ photoshop/change_to_300dpi.py
@@ -0,0 +1,13 @@
+
+from PIL import Image
+import os
+import contextlib
+
+folder = '/Users/sunshower/Documents/javascript-test/output/'
+dpi = (300, 300)
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    if filename.endswith(".png") and os.path.isfile(file_path):
+        print(filename)
+        with Image.open(file_path) as img:
+            img.save(file_path, dpi=dpi)
--- photoshop/dup_letters.js
+++ photoshop/dup_letters.js
@@ -2,14 +2,51 @@
 //check https://www.asciitable.com/
 // const s = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
 var char_range ={
-    "Numbers": "0-9",
-    "ALPHABET":"A-Z",
-    "lowercase":"a-z"
+    "Numbers": getString("0","9"),
+    "Uppercase": getString("A","Z"),
+    "Lowercase": getString("a","z"),
+    "Symbol": "%?!@&#^)\\(\"'=.*~-+$",
 }
+
+function getString(beg, end){
+    var result = "";
+    for (var i = beg.charCodeAt(0); i <= end.charCodeAt(0); i++) {
+        result += String.fromCharCode(i);
+    }
+    return result;
+}
+
+var symbolMap = {
+    "%": "percent",
+    "!": "exclamation",
+    "?": "question",
+    "@": "at",
+    "#": "hash",
+    "&": "ampersand",
+    "^": "caret",
+    ")": "close_parenthesis",
+    "/": "backslash",
+    "\\": "backslash",
+    "(": "open_parenthesis",
+    "\"": "double_quote",
+    "'": "single_quote",
+    "=": "equal",
+    ".": "dot",
+    "*": "asterisk",
+    "~": "tilde",
+    "-": "hyphen",
+    "+": "plus",
+    "$": "dollar"
+};
+
 var folder = "~/Documents/javascript-test/output/"
 
 var doc = app.activeDocument; // Make sure the document is opend and selected
 var textLayer = doc.activeLayer; //Make sure the layer that has the letter is selected
+
+// Calculate the center of the canvas
+var centerX = doc.width / 2;
+var centerY = doc.height / 2;
 
 var pngOptions = new ExportOptionsSaveForWeb();
 pngOptions.format = SaveDocumentType.PNG;
@@ -27,10 +64,17 @@ var textItem = textLayer.textItem;
 var saved = textItem.contents;
 try{
     for(var j in char_range){
-        for (var i = char_range[j].charCodeAt(0); i <= char_range[j].charCodeAt(2); i++) {
-            var character = String.fromCharCode(i);
+        for (var i = 0; i < char_range[j].length; i++) {
+            var character = char_range[j][i];
             textItem.contents = character;
-            var ch = character==='/'? "backslash": character;
+
+            var bounds = textLayer.bounds;
+            var newX = centerX - ((bounds[2].value - bounds[0].value) / 2);
+            var newY = centerY + ((bounds[1].value - bounds[3].value) / 2);
+            textLayer.translate(newX - bounds[0], newY - bounds[1]);
+            textItem.justification = Justification.CENTER;
+
+            var ch = symbolMap.hasOwnProperty(character)? symbolMap[character]: character;
             doc.exportDocument(new File(folder + j + "-" + ch + ".png"), ExportType.SAVEFORWEB, pngOptions);
         }
     }    
